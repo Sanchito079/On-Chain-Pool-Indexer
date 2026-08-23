66fab57e7c6f482f1e7a5d5f5e1b7f2dc1f52caa
Dominic Otterfi <tokenlisting79@gmail.com>
2026-08-23 09:26:08 -0700

    Configure PancakeSwap Infinity CL manager

--- src/evm/bsc/pancakeswap-infinity-constants.ts
+++ src/evm/bsc/pancakeswap-infinity-constants.ts
@@ -1,4 +1,4 @@
-export const PANCAKESWAP_INFINITY_CL_MANAGER = process.env.PANCAKESWAP_INFINITY_CL_MANAGER ?? '';
+export const PANCAKESWAP_INFINITY_CL_MANAGER = process.env.PANCAKESWAP_INFINITY_CL_MANAGER ?? '0xa0FfB9c1CE1Fe56963B0321B32E7A0302114058b';
 export const PANCAKESWAP_INFINITY_CL_MANAGER_ABI = [
   'event Initialize(bytes32 indexed id, address indexed currency0, address indexed currency1, address hooks, uint24 fee, bytes32 parameters, uint160 sqrtPriceX96, int24 tick)',
 ];
